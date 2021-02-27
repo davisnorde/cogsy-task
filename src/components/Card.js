e0fe7ae76197c1083271359650f7c124b843ce82
Davis Norde <davis.norde@gmail.com>
2021-02-27 21:53:25 +0200

    Implement menu dropdown functionality

---
+++ src/components/Card.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import thumbnail from '../assets/thumbnail.png'
+// import star from '../assets/Star.svg'
+import {ReactComponent as Star} from '../assets/Star.svg';
+import overflowMenu from '../assets/OverflowMenu.svg'
+import { Menu } from '@headlessui/react'
+
+const Card = () => {
+
+  const [isFavorite, setIsFavorite] = useState(false)
+  const handleClick = () => {
+    setIsFavorite(!isFavorite)
+  }
+
+  return (
+    <div className="mx-auto w-72 mt-12 text-dark border border-solid border-support rounded-default">
+      <div>
+        <img src={ thumbnail } alt="Card Thumbnail"/>
+      </div>
+      <div className="p-4">
+        <h1 className="text-2xl mb-4">Countdown Announcement Longer Title</h1>
+        <p className="text-sm mb-4">Last edited 2 days ago</p>
+        {isFavorite && <p>favorite</p>}
+      </div>
+      <div className="flex justify-between items-center pb-4 pr-4">
+        <button className="px-4 py-2" onClick={handleClick}>
+          <Star width="19" height="19"/>
+        </button>
+        <div className="relative">
+          <Menu>
+            <Menu.Button className="px-4 py-2 border border-solid border-support rounded-sm focus:outline-none focus:ring focus:ring-secondary active:bg-primary">
+              <img src={ overflowMenu } alt="" className="w-1"/>
+            </Menu.Button>
+            <Menu.Items className="absolute right-0 bg-white border border-support z-10 rounded-sm mt-2 focus:outline-none">
+              <Menu.Item>
+                {({active}) => (
+                  <a href="#" className={`${active ? 'bg-primary text-white' : ''} block text-sm pl-4 pr-14 leading-8`}>Save</a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+              {({active}) => (
+                  <a href="#" className={`${active ? 'bg-primary text-white' : ''} block text-sm pl-4 pr-14 leading-8`}>Edit</a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+              {({active}) => (
+                  <a href="#" className={`${active ? 'bg-primary text-white' : ''} block text-sm pl-4 pr-14 leading-8`}>Preview</a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderRadius: {
+        'default': '5px',
+      },
+      colors: {
+        'primary': '#1254ff',
+        'secondary': '#aacbff',
+        'support': '#d2d7df',
+        'dark': '#2a2941',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+    },
+  },
+  plugins: [],
+}
